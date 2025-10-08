d01f74813782463223d5972c4924bfbea4c11533
Supun Herath <supunmaduwantha400@gmail.com>
2025-10-08 18:47:59 +0530

    Test automatic deployment with GitHub Actions

--- first-next-app/app/layout.tsx
+++ first-next-app/app/layout.tsx
@@ -39,9 +39,9 @@ export default function RootLayout({
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
-              <SignUpButton>
+                            <SignUpButton>
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  supun
+                  Sign Up Now - Auto Deploy Test
                 </button>
               </SignUpButton>
             </SignedOut>
