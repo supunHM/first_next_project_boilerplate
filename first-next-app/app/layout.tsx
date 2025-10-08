65366282c19052c57346a587c8de036e23b376f9
Supun Herath <supunmaduwantha400@gmail.com>
2025-10-08 18:11:44 +0530

    supun

--- first-next-app/app/layout.tsx
+++ first-next-app/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
               <SignInButton />
               <SignUpButton>
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  fdufhudhfudhyf
+                  supun
                 </button>
               </SignUpButton>
             </SignedOut>
